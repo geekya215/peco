bff85a94b3525dcd951192cd30f6292addea9127
Lin Yang <geekya215@gmail.com>
2023-01-16 18:29:52 +0800

    use immutable data

---
+++ src/main/java/io/geekya215/peco/Func.java
@@ -0,0 +1,57 @@
+package io.geekya215.peco;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public final class Func {
+    public static Position initialPosition() {
+        return new Position(0, 0);
+    }
+
+    public static Position incrementLine(Position position) {
+        return new Position(position.line() + 1, position.column());
+    }
+
+    public static Position incrementColumn(Position position) {
+        return new Position(position.line(), position.column() + 1);
+    }
+
+    public static String currentLine(State state) {
+        var linePos = state.position().line();
+        if (linePos < state.lines().size()) {
+            return state.lines().get(linePos);
+        } else {
+            return "end of file";
+        }
+    }
+
+    public static State fromString(String str) {
+        if (str == null || str.isEmpty()) {
+            return new State(List.of(), initialPosition());
+        } else {
+            return new State(Arrays.stream(str.split("\\r?\\n")).toList(), initialPosition());
+        }
+    }
+
+    public static Tuple<State, Optional<Character>> nextChar(State state) {
+        var linePos = state.position().line();
+        var colPos = state.position().column();
+        if (linePos >= state.lines().size()) {
+            return Tuple.of(state, Optional.empty());
+        } else {
+            var curLine = currentLine(state);
+            Character c;
+            Position newPos;
+            if (colPos < curLine.length()) {
+                c = curLine.charAt(colPos);
+                newPos = incrementColumn(state.position());
+            } else {
+                c = '\n';
+                newPos = incrementLine(state.position());
+            }
+            var newState = new State(state.lines(), newPos);
+            return Tuple.of(newState, Optional.of(c));
+        }
+    }
+}
--- src/main/java/io/geekya215/peco/Position.java
+++ src/main/java/io/geekya215/peco/Position.java
@@ -1,61 +1,6 @@
 package io.geekya215.peco;
 
-import java.util.Objects;
-
-public final class Position {
-    private Integer line;
-    private Integer column;
-
-    private Position() {
-        line = 0;
-        column = 0;
-    }
-
-    public static Position initialPosition() {
-        return new Position();
-    }
-
-    public Position incrementLine() {
-        line += 1;
-        return this;
-    }
-
-    public Position incrementColumn() {
-        column += 1;
-        return this;
-    }
-
-    public Integer getLine() {
-        return line;
-    }
-
-    public Integer getColumn() {
-        return column;
-    }
-
-    @Override
-    public String toString() {
-        return "Position{" +
-                "line=" + line +
-                ", column=" + column +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Position position = (Position) o;
-
-        if (!Objects.equals(line, position.line)) return false;
-        return Objects.equals(column, position.column);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = line != null ? line.hashCode() : 0;
-        result = 31 * result + (column != null ? column.hashCode() : 0);
-        return result;
-    }
+public record Position(
+        Integer line, Integer column
+) {
 }
